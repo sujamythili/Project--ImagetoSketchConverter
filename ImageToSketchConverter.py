a95378c4899309a8d36a7f6bb29c06bdb1c2f772
sujamythili <92812427+sujamythili@users.noreply.github.com>
2021-10-19 22:27:59 +0530

    Update ImageToSketchConverter.py

--- ImageToSketchConverter.py
+++ ImageToSketchConverter.py
@@ -1,31 +1,29 @@
-import streamlit as slite
-import numpy as npy
+import streamlit as st
+import time
 import cv2
 from PIL import Image
+import numpy as np
 import io
-import time
 import base64
 
-
-
-#Left sidebar Configuration
-slite.page_set(
+st.set_page_config(
      page_title="Convert Image to Pencil Sketch",
      initial_sidebar_state="expanded",
 )
 
-#def is the keyword to define Function 
 def sketch_image_download(img,filename,text):
-    read = io.BytesIO()
-    img.save(read, format="JPEG")
-    data = base64.b64encode(read.getvalue()).decode()
-    href =  f'<a href="data:file/txt;base64,{data}" download="{filename}">{text}</a>'
+    buffered = io.BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    href =  f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
     return href
 
 def convert_image_to_sketch(img):
-    file_bytes = npy.asarray(bytearray(img), dtype=npy.uint8)
+    
+    file_bytes = np.asarray(bytearray(img), dtype=np.uint8)
     cvImage = cv2.imdecode(file_bytes, 1)
         
+    #cvImage = cv2.imread(Image.open(uploaded_file))
     cvImageGrayScale = cv2.cvtColor(cvImage, cv2.COLOR_BGR2GRAY)
     cvImageGrayScaleInversion = cv2.bitwise_not(cvImageGrayScale)
     cvImageBlured = cv2.GaussianBlur(cvImageGrayScaleInversion, (21, 21), sigmaX = 0, sigmaY = 0)
@@ -33,44 +31,44 @@ def convert_image_to_sketch(img):
     
     return sketchImage
     
-slite.title("Convert your Image to Pencil Sketch")
+st.title("Convert your Image to Pencil Sketch")
 
-slite.sidebar.title("Please Upload your image")
+st.sidebar.title("Please Upload your image")
 
-slite.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
 img = Image.open("upload.jpg")
-image = slite.image(img)
+image = st.image(img)
 
-uploaded_file = slite.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
+uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:  
     image.image(uploaded_file)
 
-if slite.sidebar.button("Convert to Pencil Sketch"):
+if st.sidebar.button("Convert to Pencil Sketch"):
      
      if uploaded_file is None:
-         slite.sidebar.error("Please upload a image to convert")
+         st.sidebar.error("Please upload a image to convert")
         
      else:
-        with slite.spinner('Converting into Pencil Sketch...'):
+        with st.spinner('Converting into Pencil Sketch...'):
             
             sketchImage = convert_image_to_sketch(uploaded_file.read())
             
             time.sleep(2)
             #image.image(sketchImage)
-            slite.success('Converted!')
-            slite.success('Click "Download Image" below the sketched image to download the image')
-            image = slite.image(sketchImage)
-            slite.sidebar.success("Please scroll down for your sketched image!")
+            st.success('Converted!')
+            st.success('Click "Download Image" below the sketched image to download the image')
+            image = st.image(sketchImage)
+            st.sidebar.success("Please scroll down for your sketched image!")
 
 
-if slite.button("Download Image"):
+if st.button("Download Image"):
     if uploaded_file:
         sketchedImage = convert_image_to_sketch(uploaded_file.read())
         image.image(sketchedImage)
         result = Image.fromarray(sketchedImage)
-        slite.success("Press the below Link")
-        slite.markdown(sketch_image_download(result,"sketched.jpg",'Download '+"Sketched.jpg"), unsafe_allow_html=True)
+        st.success("Press the below Link")
+        st.markdown(sketch_image_download(result,"sketched.jpg",'Download '+"Sketched.jpg"), unsafe_allow_html=True)
     else:
-        slite.error("Please upload a image first")
+        st.error("Please upload a image first")
